ecfc06275a0a0d29b32587251bb84b7dbe586f62
David Green <pbcub1.dg@gmail.com>
2019-05-28 23:33:35 -0600

    Initial commit

---
+++ section8/exercises/agecalc.js
@@ -0,0 +1,5 @@
+var years = prompt("What is your age?");
+var days = (years * 365) - (years/4);
+
+alert("You have been alive roughly " + days + " days.");
+console.log("You have been alive roughly " + days + " days.");
---
+++ section8/exercises/jsstalker.js
@@ -0,0 +1,9 @@
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your name is " + fName + " " + lName + ".");
+console.log("You are " + age + " years old.");
+
+alert("Your name is " + fName + " " + lName + ".");
+alert("You are " + age + " years old.");
